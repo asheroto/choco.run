9098bff9c523b2c8c0bed8bcf3c54322ac0e40f0
asheroto <github@asheroto.com>
2023-11-06 03:38:07 -0600

    Update

--- install.ps1
+++ install.ps1
@@ -20,6 +20,7 @@ Write-Output "choco.run says hello..."
 Write-Output "-------------------------------------------------------------"
 
 # Set the execution policy to RemoteSigned
+Write-Output "Changing the execution policy to RemoteSigned..."
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
 # Set the temp dir to the system temp dir
@@ -27,6 +28,7 @@ $originalPath = Get-Location
 Set-Location "$env:SystemRoot\Temp"
 
 # Install Chocolatey
+Write-Output "Installing Chocolatey..."
 $installCommand = "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 Invoke-Expression -Command $installCommand
 
@@ -34,11 +36,13 @@ Invoke-Expression -Command $installCommand
 Write-Output "Chocolatey has been installed!"
 
 # Notify the user that they can now use choco in this window
+Write-Output ""
 Write-Output "-------------------------------------------------------------"
-Write-Output "You can now use choco in this window."
+Write-Output "You can start using the 'choco' command in this window!"
 Write-Output "-------------------------------------------------------------"
 
 # Finished
+Write-Output ""
 Write-Output "-------------------------------------------------------------"
 Write-Output "choco.run has finished!"
 Write-Output "-------------------------------------------------------------"
